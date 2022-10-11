6eafcf90efbc050091ce6a80dd47d78f3affe85d
Torfa <Alexandre.Tison@etu.univ-littoral.fr>
2022-10-11 14:19:29 +0200

    Front of form in progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
-<template>
-  <div>
-    test
+<template class="">
+  <div class="w-full h-full flex justify-center items-center">
+    <form class="w-2/6 flex flex-col">
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" class="rounded-sm border-2 border-gray-300">
+      <label for="password">Mot de passe</label>
+      <input type="password" id="password" name="password" class="rounded-sm border-2 border-gray-300">
+      <button type="submit">Se connecter</button>
+    </form>
   </div>
 </template>
 
