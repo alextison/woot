8b09d21d4407490550eca222305ce4c405dace83
Torfa <Alexandre.Tison@etu.univ-littoral.fr>
2022-10-12 16:05:41 +0200

    Woot logo setup in header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="flex flex-col h-screen">
-    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800 border-b-2">
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-600 border-b-2">
       <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" class="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Woot</span>
-        </a>
+        <img src="~/assets/img/woot_logo.png" class="mr-3 h-12 sm:h-15" alt="Woot Logo" />
       </div>
     </nav>
     <main class="flex-1">
       <slot />
     </main>
-    <footer class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 border-t">
-    <span class="text-sm text-gray-500 text-center dark:text-gray-400 mx-auto">© 2022 <a href="https://flowbite.com/" class="hover:underline">Woot™</a>. All Rights Reserved.
-    </span>
+    <footer class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-600 border-t">
+      <span class="text-sm text-gray-500 text-center dark:text-gray-300 mx-auto">
+        © 2022
+        <a href="https://flowbite.com/" class="hover:underline">Woot™</a>
+        . All Rights Reserved.
+      </span>
     </footer>
   </div>
 </template>
