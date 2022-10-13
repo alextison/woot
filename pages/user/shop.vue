869a68c46d2fef8b0f1d74670c79d59e756619ef
Torfa <Alexandre.Tison@etu.univ-littoral.fr>
2022-10-13 11:51:14 +0200

    User shop completed

---
+++ pages/user/shop.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="w-full h-full flex justify-center  font-sans">
+      <div class="w-full h-full flex justify-center items-center text-center">
+        <div class="w-full h-full flex flex-col justify-center">
+            <div class="w-full flex justify-evenly items-center px-8 py-4">
+                <div class="w-1/5 h-full"></div>
+                <h2 class="w-3/5 text-4xl font-medium mb-1">Boutique</h2>
+                <div class="w-1/5 flex items-center">
+                    <p>Pièces disponibles</p>
+                    <img src="~/assets/img/coin.png" class="ml-2 mr-16 h-12" alt="Woot Coin" />
+                    <button>
+                        <svg class="svg-icon h-8" viewBox="0 0 20 20">
+                            <path d="M12.075,10.812c1.358-0.853,2.242-2.507,2.242-4.037c0-2.181-1.795-4.618-4.198-4.618S5.921,4.594,5.921,6.775c0,1.53,0.884,3.185,2.242,4.037c-3.222,0.865-5.6,3.807-5.6,7.298c0,0.23,0.189,0.42,0.42,0.42h14.273c0.23,0,0.42-0.189,0.42-0.42C17.676,14.619,15.297,11.677,12.075,10.812 M6.761,6.775c0-2.162,1.773-3.778,3.358-3.778s3.359,1.616,3.359,3.778c0,2.162-1.774,3.778-3.359,3.778S6.761,8.937,6.761,6.775 M3.415,17.69c0.218-3.51,3.142-6.297,6.704-6.297c3.562,0,6.486,2.787,6.705,6.297H3.415z"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div class="w-4/5 h-full flex justify-evenly flex-wrap mx-auto mb-4 py-2 overflow-y-auto">
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+                <div class="basis-1/5 m-2 p-1 border border-gray-500 rounded shadow-md">
+                    <img src="~/assets/img/placeholder.png" alt="Placeholder Item" />
+                    <p class="text-xl">Café gratuit</p>
+                    <p>Votre super café payé par la société</p>
+                    <button class="w-full p-1 mt-1 rounded border-2 border-indigo-400 bg-indigo-400 text-white transition duration-250 hover:bg-indigo-500 hover:border-indigo-500 disabled:opacity-50">
+                        Acheter l'élément - 5 WC
+                    </button>
+                </div>
+            </div>
+        </div>
+
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+
+</style>
